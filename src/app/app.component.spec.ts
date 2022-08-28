351ed8873dd71ad44aa3fa5ad65ed8afd6c4cea2
Axonn1018 <ktruong1018@gmail.com>
2022-08-28 03:24:18 -0400

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'site-repo-test'`, () => {
+  it(`should have as title 'site-repo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('site-repo-test');
+    expect(app.title).toEqual('site-repo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('site-repo-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('site-repo app is running!');
   });
 });
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  showDetail = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  logTest() {
+    // console.log("Button Test");
+    // this.showDetail = !this.showDetail;
+    // console.log("Current showDetail status: ", this.showDetail);
+    var x = document.getElementById("showDetail");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+}
