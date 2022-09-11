48a6bae6b8b6c3e6c4fa2192a53f0091c7fee883
Axonn1018 <ktruong1018@gmail.com>
2022-09-11 13:01:12 -0400

    Console log cleanup

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -14,20 +14,7 @@ export class HomepageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  logTest() {
-    // console.log("Button Test");
-    // this.showDetail = !this.showDetail;
-    // console.log("Current showDetail status: ", this.showDetail);
-    var x = document.getElementById("showDetail");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
   showDetailButton() {
-    console.log("Show Button Detail Is Working!")
     this.showDetail = !this.showDetail;
   }
 
