626c9b49d098b3ed128856f22410a34cc5c6cd2d
Axonn1018 <ktruong1018@gmail.com>
2022-09-12 11:39:40 -0400

    Navbar selector testing

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  pageStatus = {
+    home: false,
+    about: false,
+    contact: false
+  }
+
   constructor() { }
 
   ngOnInit(): void {
